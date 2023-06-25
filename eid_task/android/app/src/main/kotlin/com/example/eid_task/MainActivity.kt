f6e5d0d60fe9a38993b3fa781224e62440277910
mohamad amgad <mohamadamgad09@gmail.com>
2023-06-25 20:27:43 +0300

    Eid task added

---
+++ eid_task/android/app/src/main/kotlin/com/example/eid_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eid_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eid_task/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EidApp());
+}
+
+class EidApp extends StatelessWidget {
+  const EidApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true).copyWith(),
+      title: 'Eid App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  // link to generate image link : https://postimages.org/
+
+  // take Direct link from website
+  String your_image_url =
+      'https://i.postimg.cc/0yKKphby/Screenshot-from-2023-06-24-22-14-07.png';
+  // put your name
+  String your_name = 'Amgad ali';
+
+  final player = AudioPlayer();
+
+  HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF9D4EDD),
+        title: const Text(
+          "GDSC Menofia",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color(0xFF9D4EDD), Color(0xFF7B2CBF)],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 75,
+              backgroundImage: NetworkImage(your_image_url),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Happy Eid , $your_name",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            OutlinedButton(
+                onPressed: () async {
+                  await player.stop();
+                  await player.play(AssetSource('eid_sound.mp3'));
+                },
+                child: Text("Your message"))
+          ],
+        ),
+      ),
+    );
+  }
+}
